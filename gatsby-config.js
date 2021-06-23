07b3350e5928e6d24b6bcf1d8797d9f13aa2814b
Rama-Krushna <39101209+Rama-Krushna@users.noreply.github.com>
2021-06-23 23:00:32 +0530

    Metadata Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `My Trial App`,
+    title: `My React App`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@rkdotdesign`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
